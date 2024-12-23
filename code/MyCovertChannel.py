8fa77f4ebd9f3b0c7ac03609e2379d88aab15e52
Yiğit Alp Alakoç <yigitalpalakoc@gmail.com>
2024-12-23 05:24:20 +0300

    modified:   code/Example_UDPTimingInterarrivalChannelReceiver.log 	modified:   code/Example_UDPTimingInterarrivalChannelSender.log 	modified:   code/MyCovertChannel.py 	modified:   code/config.json

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -6,53 +6,49 @@ class MyCovertChannel(CovertChannelBase):
     def __init__(self):
         super().__init__()
 
-    def send(self, destination_ip, log_file_name):
-        # binary_message = self.generate_random_binary_message_with_logging(log_file_name)
-        binary_message = "msg."
-        for letter in binary_message:
+    def send(self, destination_ip, delay ,log_file_name):
+        message_send = "msg." #TODO
+        super().log_message(message_send, log_file_name)
+        for letter in message_send:
             bin_letter = ord(letter)
             for i in range (0, 8):
                 dont_fragment = bin_letter & 0b10000000
-                dont_fragment = dont_fragment >> 7
+                dont_fragment = dont_fragment >> 6
                 bin_letter = bin_letter << 1
                 packet = IP(dst=destination_ip, flags=dont_fragment) / IP()
                 super().send(packet, interface="eth0")
-                time.sleep(0.100)
+                time.sleep(delay/1000)
 
     def receive(self, source_ip, log_file_name):
-        """
-        - Listen for incoming packets, decode the message based on the 'Don't Fragment' flag in the IP header, and log the received message.
-        """
-        # Initialize a variable to store the received binary message
-        binary_message = ""
+        message_receive = ""
+        msg = 0
         ctr = 0
-
+        stop_sniffing = False
         def packet_handler(packet):
-            nonlocal binary_message
+            nonlocal message_receive, msg, ctr, stop_sniffing
 
-            # Filter packets from the expected source and with an IP layer
-            if packet.haslayer(IP) and packet[IP].src == source_ip:  # Assuming parameter1 is the source IP
-                # Extract the 'Don't Fragment' flag (1 or 0)
+            if packet.haslayer(IP) and packet[IP].src == source_ip:
+                # rint(f"Packet flags: {packet[IP].flags}")
                 dont_fragment_flag = (packet[IP].flags & 0x2) >> 1
-
-                # Append the extracted bit to the binary message
-                binary_message += str(dont_fragment_flag)
+                msg += dont_fragment_flag
+                ctr += 1
+                if ctr < 8:
+                    msg *= 2
+                elif ctr == 8:
+                    letter = chr(msg)
+                    # print(msg)
+                    # print(letter)
+                    message_receive += letter
+                    msg = 0
+                    ctr = 0
+                    if letter == '.':
+                        stop_sniffing = True
 
         def stop_filter(packet):
-            nonlocal binary_message
-            # Check if the transmission ends (binary for '.')
-            return binary_message.endswith("00101110")  # Binary for '.'
+            return stop_sniffing
 
-        # Capture packets until the message is fully received
         sniff(filter=f"ip src {source_ip}", prn=packet_handler, stop_filter=stop_filter)
 
-        # Convert the binary message to a string
-        decoded_message = "".join(
-            self.convert_eight_bits_to_character(binary_message[i:i+8])
-            for i in range(0, len(binary_message), 8)
-        )
+        self.log_message(message_receive, log_file_name)
 
-        # Log the decoded message
-        self.log_message(decoded_message, log_file_name)
-
-        return decoded_message
+        return message_receive
