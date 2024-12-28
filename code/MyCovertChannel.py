1cc8b7229e3aaac1ee13a10595a6a773032e3ae6
altayucetas <e2540367@ceng.metu.edu.tr>
2024-12-28 21:59:58 +0300

    Encryption is added.

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -6,29 +6,40 @@ class MyCovertChannel(CovertChannelBase):
     def __init__(self):
         super().__init__()
 
-    def send(self, destination_ip, delay ,log_file_name):
+    def xor(self, inp_str, key):
+        if isinstance(inp_str, str):
+            inp_str = inp_str.encode()  # Convert to bytes if it's a string
+        key_len = len(key)
+        return bytes([inp_str[i] ^ ord(key[i % key_len]) for i in range(len(inp_str))])
+
+    def send(self, destination_ip, delay, log_file_name):
         message_send = super().generate_random_message()
         len_msg = len(message_send)
-        super().log_message(message_send, log_file_name)
+        
+        # Encrypt the message
+        super().log_message(message_send, log_file_name) # Log encrypted message as hex for clarity
+        enc_msg = self.xor(message_send[:-1], "key") + b"."
+        
         start = time.time()
-        for letter in message_send:
-            bin_letter = ord(letter)
-            for i in range (0, 8):
+        for byte in enc_msg:
+            bin_letter = byte
+            for i in range(8):
                 flag = bin_letter & 0b10000000
                 flag = flag >> 6
                 bin_letter = bin_letter << 1
                 packet = IP(dst=destination_ip, flags=flag) / IP()
                 super().send(packet, interface="eth0")
-                time.sleep(delay/1000)
+                time.sleep(delay / 1000)
         end = time.time()
         length = end - start
-        print(len_msg*8,"packets in", length, "seconds, avg = ", (len_msg*8)/length, "packets (bits) / second.")
+        print(len_msg * 8, "packets in", length, "seconds, avg =", (len_msg * 8) / length, "packets (bits) / second.")
 
     def receive(self, source_ip, log_file_name):
-        message_receive = ""
+        message_receive = b""
         msg = 0
         ctr = 0
         stop_sniffing = False
+
         def packet_handler(packet):
             nonlocal message_receive, msg, ctr, stop_sniffing
 
@@ -40,13 +51,10 @@ class MyCovertChannel(CovertChannelBase):
                 if ctr < 8:
                     msg *= 2
                 elif ctr == 8:
-                    letter = chr(msg)
-                    print(msg)
-                    print(letter)
-                    message_receive += letter
+                    message_receive += bytes([msg])
                     msg = 0
                     ctr = 0
-                    if letter == '.':
+                    if message_receive.endswith(b"."):
                         stop_sniffing = True
 
         def stop_filter(packet):
@@ -54,6 +62,8 @@ class MyCovertChannel(CovertChannelBase):
 
         sniff(filter=f"ip src {source_ip}", prn=packet_handler, stop_filter=stop_filter)
 
-        self.log_message(message_receive, log_file_name)
+        # Decrypt the received message
+        decrypted_message = self.xor(message_receive[:-1], "key").decode() + "."
+        self.log_message(decrypted_message, log_file_name)
 
         return message_receive
